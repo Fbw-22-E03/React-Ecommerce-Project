82851089bf97d044799e0ec9d6dbb0dd912ff149
Sunny Eyles <sunnyeyles@gmail.com>
2022-10-20 11:36:32 +0200

    Progress on login

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -7,11 +7,9 @@ import Alert from "react-bootstrap/Alert";
 
 const Wrapper = styled.div`
   max-width: 28rem;
-  margin: 2rem auto;
-
+  margin: 10rem auto;
 
   border: solid 1px rgb(0, 0, 0);
-
 
   width: 80vw;
   // border: solid 1px rgb(0, 0, 0);
@@ -20,8 +18,6 @@ const Wrapper = styled.div`
   background-color: rgba(255, 255, 255);
   box-shadow: 0px 0px 15px -2px rgba(0, 0, 0, 0.2),
     9px 9px 15px -2px rgba(0, 0, 0, 0.1);
-
-
 `;
 const Header = styled.h2`
   margin: 2rem auto 3rem;
@@ -56,38 +52,38 @@ function Login() {
   }
 
   return (
-      <Wrapper>
-        <Alert
-          ref={show}
-          style={{ display: "none" }}
-          variant="danger"
-          onClose={() => (show.current.style.display = "none")}
-          dismissible
-        >
-          {/* <Alert.Heading>Whoops</Alert.Heading> */}
-          <p>Incorrect email or password</p>
-        </Alert>
-        <Header>LOGIN</Header>
-        <form onSubmit={submitData} onChange={handleChange}>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">
-              Email
-            </label>
-            <input type="email" name="email" className="form-control" />
-            <div className="form-text"></div>
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input type="password" name="password" className="form-control" />
-          </div>
-          <button type="submit" className="login-btn btn btn-outline-dark">
-            Log in
-          </button>
-        </form>
-        <Link className="login-link" to="/Registration">
-          Not registered yet? Click here.
-        </Link>
-      </Wrapper>
+    <Wrapper>
+      <Alert
+        ref={show}
+        style={{ display: "none" }}
+        variant="danger"
+        onClose={() => (show.current.style.display = "none")}
+        dismissible
+      >
+        {/* <Alert.Heading>Whoops</Alert.Heading> */}
+        <p>Incorrect email or password</p>
+      </Alert>
+      <Header>LOGIN</Header>
+      <form onSubmit={submitData} onChange={handleChange}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email
+          </label>
+          <input type="email" name="email" className="form-control" />
+          <div className="form-text"></div>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input type="password" name="password" className="form-control" />
+        </div>
+        <button type="submit" className="login-btn btn btn-outline-dark">
+          Log in
+        </button>
+      </form>
+      <Link className="login-link" to="/Registration">
+        Not registered yet? Click here.
+      </Link>
+    </Wrapper>
   );
 }
 
